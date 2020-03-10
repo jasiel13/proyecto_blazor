c6527cfb1b790710ea6ed6040e36b2d374c39931
Jasiel Becerra <54915798+jasiel13@users.noreply.github.com>
2020-03-10 12:56:19 -0600

    Primera actualizacion

---
+++ BlazorCRUD.Model/Film.cs
@@ -0,0 +1,14 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorCRUD.Model
+{
+    public class Film
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public string director { get; set; }
+        public DateTime releasedate { get; set; }
+    }
+}
