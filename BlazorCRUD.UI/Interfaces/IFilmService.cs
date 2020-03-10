0df72d63a96f4cdf0cf6f598880fdd17bf96a2f4
Jasiel Becerra <54915798+jasiel13@users.noreply.github.com>
2020-03-10 12:57:53 -0600

    BlazorUI

---
+++ BlazorCRUD.UI/Interfaces/IFilmService.cs
@@ -0,0 +1,17 @@
+﻿using BlazorCRUD.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorCRUD.UI.Interfaces
+{
+    //metodos para la interface de servicios
+    public interface IFilmService
+    {
+        Task<IEnumerable<Film>> GetAllFilms();
+        Task<Film>GetDetails(int id);
+        Task<bool> DeleteFilm(int id);
+        Task<bool> SaveFilm(Film film);
+    }
+}
